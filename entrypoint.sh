a5fa46cf04045c598e4868ba4893337cb2b0d1c0
minakoari <marcellocord54@gmail.com>
2021-03-26 06:15:22 -0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,34 +2,37 @@
 
 set -e
 
-usage()
-{
-cat << EOF
-usage: ./entrypoint.sh --type=test_type --extra=extra_parameters
--t    | --type  (required)  Test type
--e    | --extra (optional)  Extra parameters
--h    | --help
-EOF
-}
+# usage()
+# {
+# cat << EOF
+# usage: ./entrypoint.sh --type=test_type --extra=extra_parameters
+# -t    | --type  (required)  Test type
+# -e    | --extra (optional)  Extra parameters
+# -h    | --help
+# EOF
+# }
 
-for i in "$@"
-do
-    case $i in
-        -t=*|--type=*)
-            TYPE="${i#*=}"
-            shift # past argument=value
-        ;;
+# for i in "$@"
+# do
+#     case $i in
+#         -t=*|--type=*)
+#             TYPE="${i#*=}"
+#             shift # past argument=value
+#         ;;
         
-        -e=*|--extra=*)
-            EXTRA="${i#*=}"
-            shift # past argument=value
-        ;;
+#         -e=*|--extra=*)
+#             EXTRA="${i#*=}"
+#             shift # past argument=value
+#         ;;
         
-        *)
-            # unknown option
-        ;;
-    esac
-done
+#         *)
+#             # unknown option
+#         ;;
+#     esac
+# done
+
+TYPE = $INPUT_TEST_TYPE
+EXTRA = $INPUT_EXTRA_ARGUMENTS
 
 if [ -z "$TYPE" ]
 then
