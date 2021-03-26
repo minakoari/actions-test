80c2a89aec08152b01da380104da67b453255a61
minakoari <marcellocord54@gmail.com>
2021-03-26 03:43:25 -0300

    Simple arguments

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,16 @@ FROM ubuntu:latest
 
 ENV DEBIAN_FRONTEND=noninteractive 
 
-RUN apt-get update && apt-get install -y python3-pip openjdk-11-jdk maven stress-ng
+# Python support
+RUN apt-get update && apt-get install -y python3-pip
 RUN python3 -m pip install --upgrade pip
 RUN python3 -m pip install pytest
 
-COPY entrypoint.sh /
+# Java/Maven support
+RUN apt-get update && apt-get install -y openjdk-11-jdk maven
+
+RUN apt-get update && apt-get install -y stress-ng
+
+COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,53 @@
 
 set -e
 
-sh -c "$*"
+usage()
+{
+cat << EOF
+usage: ./entrypoint.sh --type=test_type --extra=extra_parameters
+-t    | --type  (required)  Test type
+-e    | --extra (optional)  Extra parameters
+-h    | --help
+EOF
+}
+
+for i in "$@"
+do
+    case $i in
+        -t=*|--type=*)
+            TYPE="${i#*=}"
+            shift # past argument=value
+        ;;
+        
+        -e=*|--extra=*)
+            EXTRA="${i#*=}"
+            shift # past argument=value
+        ;;
+        
+        *)
+            # unknown option
+        ;;
+    esac
+done
+
+if [ -z "$TYPE" ]
+then
+    usage
+    exit
+fi
+
+case $TYPE in
+    pytest)
+        sh -c pytest $EXTRA
+    ;;
+    
+    maven)
+        sh -c mvn test $EXTRA -q
+    ;;
+    
+    *)
+        echo -n "unknown"
+    ;;
+esac
+
+# sh -c "$*"
